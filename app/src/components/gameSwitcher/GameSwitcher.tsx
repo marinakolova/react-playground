f17c39251bae2b42cde4c456c833a4fc45b56ad7
marinakolova <marina.kolova.359@gmail.com>
2021-12-12 20:17:43 +0200

    added Tic-Tac-Toe game

---
+++ app/src/components/gameSwitcher/GameSwitcher.tsx
@@ -0,0 +1,21 @@
+import * as React from 'react';
+import { useState } from 'react';
+import { TicTacToe } from '../games/ticTacToe/TicTacToe';
+import { NavBar } from './navBar/NavBar';
+
+export interface IGameSwitcherProps {
+}
+
+export function GameSwitcher(props: IGameSwitcherProps) {
+  const [selected, setSelected] = useState<number>(0);
+
+  return (
+    <>
+      <NavBar
+        selected={selected}
+        setSelected={setSelected}
+      />
+      {selected === 0 && <TicTacToe />}
+    </>
+  );
+}
